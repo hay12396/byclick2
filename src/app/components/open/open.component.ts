1d6748a5e0de6cd83d91985b877dacdf2d691046
voyager1302 <yehuda.shimshon@gmail.com>
2018-11-01 14:39:31 +0200

    # 7 fix Responsive up to 750

--- src/app/components/open/open.component.ts
+++ src/app/components/open/open.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {Subscription} from 'rxjs';
-import {MediaChange, ObservableMedia} from '@angular/flex-layout';
-import {MessengerService} from '../../services/messenger.service';
 import * as jsPDF  from 'jspdf';
 
 @Component({
@@ -10,28 +7,12 @@ import * as jsPDF  from 'jspdf';
   templateUrl: './open.component.html',
   styleUrls: ['./open.component.scss']
 })
-export class OpenComponent implements OnInit, OnDestroy {
-  watcher: Subscription;
-  activeMediaQuery = '';
-  am = 'xs';
-  height = '';
+export class OpenComponent implements OnInit {
 
+  constructor( private router: Router){ }
 
-  constructor(media: ObservableMedia, private router: Router, private messengerService: MessengerService) {
-    this.watcher = media.subscribe((change: MediaChange) => {
-      this.activeMediaQuery = change.mqAlias || '';
-      console.log(`Media query --> ${change.mqAlias}`);
-    });
-  }
+  ngOnInit() { }
 
-  ngOnInit() {
-    const elm = document.getElementById('open-container');
-    console.log(window.screen.availHeight);
-  }
-
-  ngOnDestroy() {
-    this.watcher.unsubscribe();
-  }
 
   startTheForm(): void {
     this.router.navigate(['/personal-data']);
